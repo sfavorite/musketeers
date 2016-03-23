b7ac90b3a736801b567c5220a953de6d1bc9d125
Scot Favorite <scot.favorite@gmail.com>
2016-03-23 13:40:22 -0400

    Increased text file size.

--- src/Musketeers.php
+++ src/Musketeers.php
@@ -7,19 +7,32 @@
 
         public function saySomething($number = 1) {
 
+            /*
             $output_text = '';
             for ($i = 1; $i <= $number; $i++)
             {
                 $output_text = '</p>' . $output_text . $this->paragraphs() . '</p>';
 
             }
-
             return $output_text;
+            */
+            return $this->paragraphs();
         }
 
         private function paragraphs()
         {
+            $paragraphs = array();
+            //$path = dirname(dirname(__FILE__)) . '/src/';
+            $path = dirname(__DIR__) . '/src/';
+            $filename = 'musketeers.txt';
+            $fp = @fopen($path . $filename, 'r');
 
+            if ($fp) {
+                $paragraphs = explode("\n\n", fread($fp, filesize($path . $filename)));
+            } else {
+                echo 'Error';
+            }
+            /*
             return 'On the first Monday of the month of April, 1625, the market town of
                 Meung, in which the author of ROMANCE OF THE ROSE was born, appeared to
                 be in as perfect a state of revolution as if the Huguenots had just made
@@ -29,6 +42,8 @@
                 courage with a musket or a partisan, directed their steps toward the
                 hostelry of the Jolly Miller, before which was gathered, increasing
                 every minute, a compact group, vociferous and full of curiosity.';
+            */
+            return $paragraphs;
         }
 
         public static function showImage()
